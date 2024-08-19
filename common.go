99b0f6476b3c0ac65d0ca6ff4a65227f49558a55
Patrick Tran <pho.tran@productaddon.com>
2024-08-19 14:40:52 +0700

    Add fields to ToAppResponse model

--- common.go
+++ common.go
@@ -44,35 +44,41 @@ type Data struct {
 	FileURL string `json:"fileURL"`
 
 	//------------------Array-----------------
-	ObjectList             []bson.M                     `json:"objectList"`
-	QueueIncomingList      []Queue                      `json:"queueIncomingList"`
-	QueueOutgoingList      []Queue                      `json:"queueOutgoingList"`
-	QueueSFTPList          []Queue                      `json:"queueSFTPList"`
-	QueueWebhookList       []Queue                      `json:"queueWebhookList"`
-	UserDefinedTableList   []UserDefinedTable           `json:"userDefinedTableList"`
-	ApplicationList        []Application                `json:"applicationList"`
-	IntegrationFlowList    []IntegrationFlow            `json:"integrationFlowList"`
-	QueueStatusList        []ReportQueueStatus          `json:"queueStatusList"`
-	DataMapperList         []DataMapper                 `json:"dataMapperList"`
-	TenantList             []Tenant                     `json:"tenantList"`
-	MicroServiceList       []MicroServiceResponse       `json:"microServiceList"`
-	MicroServiceStatusList []MicroServiceStatusResponse `json:"microServiceStatusList"`
+	ObjectList                        []bson.M                        `json:"objectList"`
+	QueueIncomingList                 []Queue                         `json:"queueIncomingList"`
+	QueueOutgoingList                 []Queue                         `json:"queueOutgoingList"`
+	QueueSFTPList                     []Queue                         `json:"queueSFTPList"`
+	QueueWebhookList                  []Queue                         `json:"queueWebhookList"`
+	UserDefinedTableList              []UserDefinedTable              `json:"userDefinedTableList"`
+	ApplicationList                   []Application                   `json:"applicationList"`
+	IntegrationFlowList               []IntegrationFlow               `json:"integrationFlowList"`
+	QueueStatusList                   []ReportQueueStatus             `json:"queueStatusList"`
+	DataMapperList                    []DataMapper                    `json:"dataMapperList"`
+	TenantList                        []Tenant                        `json:"tenantList"`
+	MicroServiceList                  []MicroServiceResponse          `json:"microServiceList"`
+	MicroServiceStatusList            []MicroServiceStatusResponse    `json:"microServiceStatusList"`
+	DirectIntegrationFlowList         []DirectIntegrationFlow         `json:"directIntegrationFlowList"`
+	DirectIntegrationExecutionList    []DirectIntegrationExecution    `json:"directIntegrationExecutionList"`
+	DirectIntegrationExecutionLogList []DirectIntegrationExecutionLog `json:"directIntegrationExecutionLogList"`
 
 	//------------------Object-----------------
 
-	Object             bson.M                     `json:"object"`
-	QueueIncoming      Queue                      `json:"queueIncoming"`
-	QueueOutgoing      Queue                      `json:"queueOutgoing"`
-	QueueSFTP          Queue                      `json:"queueSFTP"`
-	QueueWebhook       Queue                      `json:"queueWebhook"`
-	UserDefinedTable   UserDefinedTable           `json:"userDefinedTable"`
-	Application        Application                `json:"application"`
-	DataOut            string                     `json:"dataOut"`
-	DataMapper         DataMapper                 `json:"dataMapper"`
-	IntegrationFlow    IntegrationFlow            `json:"integrationFlow"`
-	Tenant             Tenant                     `json:"tenant"`
-	MicroService       MicroServiceResponse       `json:"microService"`
-	MicroServiceStatus MicroServiceStatusResponse `json:"microServiceStatus"`
+	Object                        bson.M                        `json:"object"`
+	QueueIncoming                 Queue                         `json:"queueIncoming"`
+	QueueOutgoing                 Queue                         `json:"queueOutgoing"`
+	QueueSFTP                     Queue                         `json:"queueSFTP"`
+	QueueWebhook                  Queue                         `json:"queueWebhook"`
+	UserDefinedTable              UserDefinedTable              `json:"userDefinedTable"`
+	Application                   Application                   `json:"application"`
+	DataOut                       string                        `json:"dataOut"`
+	DataMapper                    DataMapper                    `json:"dataMapper"`
+	IntegrationFlow               IntegrationFlow               `json:"integrationFlow"`
+	Tenant                        Tenant                        `json:"tenant"`
+	MicroService                  MicroServiceResponse          `json:"microService"`
+	MicroServiceStatus            MicroServiceStatusResponse    `json:"microServiceStatus"`
+	DirectIntegrationFlow         DirectIntegrationFlow         `json:"directIntegrationFlow"`
+	DirectIntegrationExecution    DirectIntegrationExecution    `json:"directIntegrationExecution"`
+	DirectIntegrationExecutionLog DirectIntegrationExecutionLog `json:"directIntegrationExecutionLog"`
 }
 
 type DenominationRequest struct {
