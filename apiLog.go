8339855fe3fa019b2bb426ebf03b3e92c7844790
Patrick Tran <pho.tran@productaddon.com>
2024-08-24 17:45:07 +0700

    Add APILog model

---
+++ apiLog.go
@@ -0,0 +1,22 @@
+package Model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type APILog struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name" bson:"name"`
+	Method    string             `json:"method" bson:"method"`
+	SourceIP  string             `json:"sourceIP" bson:"sourceIP"`
+	StartDate int64              `json:"startDate" bson:"startDate"`
+	EndDate   int64              `json:"endDate" bson:"endDate"`
+	TenantID  string             `json:"tenantID" bson:"tenantID"`
+}
+
+type APILogRequest struct {
+	Name      string `json:"name" bson:"name"`
+	Method    string `json:"method" bson:"method"`
+	SourceIP  string `json:"sourceIP" bson:"sourceIP"`
+	StartDate int64  `json:"startDate" bson:"startDate"`
+	EndDate   int64  `json:"endDate" bson:"endDate"`
+	TenantID  string `json:"tenantID" bson:"tenantID"`
+}
