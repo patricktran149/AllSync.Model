93a825a8aafe68d5deea03960a984752da20e921
Patrick Tran <pho.tran@productaddon.com>
2024-08-24 17:49:26 +0700

    Add CreatedDate to APILog

--- apiLog.go
+++ apiLog.go
@@ -3,13 +3,14 @@ package Model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type APILog struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name      string             `json:"name" bson:"name"`
-	Method    string             `json:"method" bson:"method"`
-	SourceIP  string             `json:"sourceIP" bson:"sourceIP"`
-	StartDate int64              `json:"startDate" bson:"startDate"`
-	EndDate   int64              `json:"endDate" bson:"endDate"`
-	TenantID  string             `json:"tenantID" bson:"tenantID"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"name" bson:"name"`
+	Method      string             `json:"method" bson:"method"`
+	SourceIP    string             `json:"sourceIP" bson:"sourceIP"`
+	StartDate   int64              `json:"startDate" bson:"startDate"`
+	EndDate     int64              `json:"endDate" bson:"endDate"`
+	TenantID    string             `json:"tenantID" bson:"tenantID"`
+	CreatedDate int64              `json:"createdDate" bson:"createdDate"`
 }
 
 type APILogRequest struct {
