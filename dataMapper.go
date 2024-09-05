00371fde3891134d1cc04c2e527d11a895686f75
Patrick Tran <pho.tran@productaddon.com>
2024-09-05 13:40:14 +0700

    Add TargetList into Data Mapper

--- dataMapper.go
+++ dataMapper.go
@@ -7,36 +7,39 @@ import (
 )
 
 type DataMapper struct {
-	ID            primitive.ObjectID     `json:"_id,omitempty" bson:"_id,omitempty"`
-	ApplicationID string                 `json:"applicationID" bson:"applicationID"`
-	TableName     string                 `json:"tableName" bson:"tableName"`
-	Template      string                 `json:"template" bson:"template"`
-	Direction     DataMapperDirection    `json:"direction" bson:"direction"`
-	Data          map[string]interface{} `json:"data" bson:"data"`
-	CreatedDate   int64                  `json:"createdDate" bson:"createdDate"`
-	CreatedBy     string                 `json:"createdBy" bson:"createdBy"`
-	UpdatedDate   int64                  `json:"updatedDate" bson:"updatedDate"`
-	UpdatedBy     string                 `json:"updatedBy" bson:"updatedBy"`
+	ID            primitive.ObjectID      `json:"_id,omitempty" bson:"_id,omitempty"`
+	ApplicationID string                  `json:"applicationID" bson:"applicationID"`
+	TableName     string                  `json:"tableName" bson:"tableName"`
+	Template      string                  `json:"template" bson:"template"`
+	Direction     DataMapperDirection     `json:"direction" bson:"direction"`
+	TargetList    []IntegrationFlowConfig `json:"targetList" bson:"targetList"`
+	Data          map[string]interface{}  `json:"data" bson:"data"`
+	CreatedDate   int64                   `json:"createdDate" bson:"createdDate"`
+	CreatedBy     string                  `json:"createdBy" bson:"createdBy"`
+	UpdatedDate   int64                   `json:"updatedDate" bson:"updatedDate"`
+	UpdatedBy     string                  `json:"updatedBy" bson:"updatedBy"`
 }
 
 type DataMapperRequest struct {
-	ID            primitive.ObjectID     `json:"_id,omitempty" bson:"_id,omitempty"`
-	ApplicationID string                 `json:"applicationID" bson:"applicationID"`
-	TableName     string                 `json:"tableName" bson:"tableName"`
-	Template      string                 `json:"template" bson:"template"`
-	Direction     DataMapperDirection    `json:"direction" bson:"direction"`
-	Data          map[string]interface{} `json:"data" bson:"data"`
-	CreatedBy     string                 `json:"createdBy" bson:"createdBy"`
+	ID            primitive.ObjectID      `json:"_id,omitempty" bson:"_id,omitempty"`
+	ApplicationID string                  `json:"applicationID" bson:"applicationID"`
+	TableName     string                  `json:"tableName" bson:"tableName"`
+	Template      string                  `json:"template" bson:"template"`
+	Direction     DataMapperDirection     `json:"direction" bson:"direction"`
+	TargetList    []IntegrationFlowConfig `json:"targetList" bson:"targetList"`
+	Data          map[string]interface{}  `json:"data" bson:"data"`
+	CreatedBy     string                  `json:"createdBy" bson:"createdBy"`
 }
 
 type DataMapperUpdateRequest struct {
-	ID            primitive.ObjectID     `json:"_id,omitempty" bson:"_id,omitempty"`
-	ApplicationID string                 `json:"applicationID" bson:"applicationID"`
-	TableName     string                 `json:"tableName" bson:"tableName"`
-	Template      string                 `json:"template" bson:"template"`
-	Direction     DataMapperDirection    `json:"direction" bson:"direction"`
-	Data          map[string]interface{} `json:"data" bson:"data"`
-	UpdatedBy     string                 `json:"updatedBy" bson:"updatedBy"`
+	ID            primitive.ObjectID      `json:"_id,omitempty" bson:"_id,omitempty"`
+	ApplicationID string                  `json:"applicationID" bson:"applicationID"`
+	TableName     string                  `json:"tableName" bson:"tableName"`
+	Template      string                  `json:"template" bson:"template"`
+	Direction     DataMapperDirection     `json:"direction" bson:"direction"`
+	TargetList    []IntegrationFlowConfig `json:"targetList" bson:"targetList"`
+	Data          map[string]interface{}  `json:"data" bson:"data"`
+	UpdatedBy     string                  `json:"updatedBy" bson:"updatedBy"`
 }
 
 type DataMapperDirection string
