a8861d234e9d3632cf945922e9b83f18db78344b
Patrick Tran <pho.tran@productaddon.com>
2024-09-23 12:24:16 +0700

    Add mongoCollectionName to QueryManager model

--- queryManager.go
+++ queryManager.go
@@ -5,28 +5,31 @@ import (
 )
 
 type QueryManager struct {
-	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	QueryName   string             `json:"queryName" bson:"queryName"`
-	SQLQuery    string             `json:"sqlQuery" bson:"sqlQuery"`
-	MongoQuery  string             `json:"mongoQuery" bson:"mongoQuery"`
-	CreatedBy   string             `json:"createdBy" bson:"createdBy"`
-	CreatedDate int64              `json:"createdDate" bson:"createdDate"`
-	UpdatedBy   string             `json:"updatedBy" bson:"updatedBy"`
-	UpdatedDate int64              `json:"updatedDate" bson:"updatedDate"`
+	ID                  primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	QueryName           string             `json:"queryName" bson:"queryName"`
+	SQLQuery            string             `json:"sqlQuery" bson:"sqlQuery"`
+	MongoCollectionName string             `json:"mongoCollectionName" bson:"mongoCollectionName"`
+	MongoQuery          string             `json:"mongoQuery" bson:"mongoQuery"`
+	CreatedBy           string             `json:"createdBy" bson:"createdBy"`
+	CreatedDate         int64              `json:"createdDate" bson:"createdDate"`
+	UpdatedBy           string             `json:"updatedBy" bson:"updatedBy"`
+	UpdatedDate         int64              `json:"updatedDate" bson:"updatedDate"`
 }
 
 type QueryManagerRequest struct {
-	QueryName  string `json:"queryName" bson:"queryName"`
-	SQLQuery   string `json:"sqlQuery" bson:"sqlQuery"`
-	MongoQuery string `json:"mongoQuery" bson:"mongoQuery"`
-	CreatedBy  string `json:"createdBy" bson:"createdBy"`
-	UpdatedBy  string `json:"updatedBy" bson:"updatedBy"`
+	QueryName           string `json:"queryName" bson:"queryName"`
+	SQLQuery            string `json:"sqlQuery" bson:"sqlQuery"`
+	MongoCollectionName string `json:"mongoCollectionName" bson:"mongoCollectionName"`
+	MongoQuery          string `json:"mongoQuery" bson:"mongoQuery"`
+	CreatedBy           string `json:"createdBy" bson:"createdBy"`
+	UpdatedBy           string `json:"updatedBy" bson:"updatedBy"`
 }
 
 type QueryManagerUpdateRequest struct {
-	QueryName  string `json:"queryName" bson:"queryName"`
-	SQLQuery   string `json:"sqlQuery" bson:"sqlQuery"`
-	MongoQuery string `json:"mongoQuery" bson:"mongoQuery"`
-	CreatedBy  string `json:"createdBy" bson:"createdBy"`
-	UpdatedBy  string `json:"updatedBy" bson:"updatedBy"`
+	QueryName           string `json:"queryName" bson:"queryName"`
+	SQLQuery            string `json:"sqlQuery" bson:"sqlQuery"`
+	MongoCollectionName string `json:"mongoCollectionName" bson:"mongoCollectionName"`
+	MongoQuery          string `json:"mongoQuery" bson:"mongoQuery"`
+	CreatedBy           string `json:"createdBy" bson:"createdBy"`
+	UpdatedBy           string `json:"updatedBy" bson:"updatedBy"`
 }
