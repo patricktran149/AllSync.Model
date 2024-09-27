422a62fee35cf7c6fdc7dba7315463f4645ff12c
Patrick Tran <pho.tran@productaddon.com>
2024-09-27 11:55:33 +0700

    Fix ID

--- oAuth2Token.go
+++ oAuth2Token.go
@@ -3,14 +3,15 @@ package Model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type OAuth2Token struct {
-	ID            primitive.ObjectID `json:"_id" bson:"_id"`
+	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	ApplicationID string             `json:"applicationID" bson:"applicationID"`
 	Token         Token              `json:"token" bson:"token"`
 	FreshToken    Token              `json:"freshToken" bson:"freshToken"`
 }
 
 type OAuth2TokenRequest struct {
-	ApplicationID string `json:"applicationID" bson:"applicationID"`
-	Token         Token  `json:"token" bson:"token"`
-	FreshToken    Token  `json:"freshToken" bson:"freshToken"`
+	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	ApplicationID string             `json:"applicationID" bson:"applicationID"`
+	Token         Token              `json:"token" bson:"token"`
+	FreshToken    Token              `json:"freshToken" bson:"freshToken"`
 }
