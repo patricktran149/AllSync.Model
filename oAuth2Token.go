4ea3aa1f4b41701c6040c17797c2f3f25d6127a8
Patrick Tran <pho.tran@productaddon.com>
2024-09-29 12:58:57 +0700

    Add OAuth2TokenUpdateRequest model

--- oAuth2Token.go
+++ oAuth2Token.go
@@ -10,8 +10,13 @@ type OAuth2Token struct {
 }
 
 type OAuth2TokenRequest struct {
-	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	ApplicationID string             `json:"applicationID" bson:"applicationID"`
-	Token         Token              `json:"token" bson:"token"`
-	FreshToken    Token              `json:"freshToken" bson:"freshToken"`
+	ApplicationID string `json:"applicationID" bson:"applicationID"`
+	Token         Token  `json:"token" bson:"token"`
+	FreshToken    Token  `json:"freshToken" bson:"freshToken"`
+}
+
+type OAuth2TokenUpdateRequest struct {
+	ApplicationID string `json:"applicationID" bson:"applicationID"`
+	Token         Token  `json:"token" bson:"token"`
+	FreshToken    Token  `json:"freshToken" bson:"freshToken"`
 }
