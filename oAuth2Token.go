61971f22a07ce17a831a1fc94629b2080e268324
Patrick Tran <pho.tran@productaddon.com>
2024-11-15 11:24:22 +0700

    Add lastSentEmailDate to main and update request OAuth2Token model

--- oAuth2Token.go
+++ oAuth2Token.go
@@ -3,11 +3,12 @@ package Model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type OAuth2Token struct {
-	ID               primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	ApplicationID    string             `json:"applicationID" bson:"applicationID"`
-	Token            Token              `json:"token" bson:"token"`
-	FreshToken       Token              `json:"freshToken" bson:"freshToken"`
-	TokenExpiryEmail string             `json:"tokenExpiryEmail" bson:"tokenExpiryEmail"`
+	ID                primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	ApplicationID     string             `json:"applicationID" bson:"applicationID"`
+	Token             Token              `json:"token" bson:"token"`
+	FreshToken        Token              `json:"freshToken" bson:"freshToken"`
+	TokenExpiryEmail  string             `json:"tokenExpiryEmail" bson:"tokenExpiryEmail"`
+	LastSentEmailDate int64              `json:"lastSentEmailDate" bson:"lastSentEmailDate"`
 }
 
 type OAuth2TokenRequest struct {
@@ -18,8 +19,9 @@ type OAuth2TokenRequest struct {
 }
 
 type OAuth2TokenUpdateRequest struct {
-	ApplicationID    string `json:"applicationID" bson:"applicationID"`
-	Token            Token  `json:"token" bson:"token"`
-	FreshToken       Token  `json:"freshToken" bson:"freshToken"`
-	TokenExpiryEmail string `json:"tokenExpiryEmail" bson:"tokenExpiryEmail"`
+	ApplicationID     string `json:"applicationID" bson:"applicationID"`
+	Token             Token  `json:"token" bson:"token"`
+	FreshToken        Token  `json:"freshToken" bson:"freshToken"`
+	TokenExpiryEmail  string `json:"tokenExpiryEmail" bson:"tokenExpiryEmail"`
+	LastSentEmailDate int64  `json:"lastSentEmailDate" bson:"lastSentEmailDate"`
 }
