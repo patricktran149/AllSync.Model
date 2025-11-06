a702e70cbebb715bf084571557c0702a4b16e106
Patrick Tran <pho.tran@productaddon.com>
2025-11-06 15:00:08 +0700

    Add createdBy, createDate, updatedBy, updatedDate to GlobalVariable model

--- globalVariable.go
+++ globalVariable.go
@@ -6,16 +6,22 @@ type GlobalVariable struct {
 	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	VariableID    string             `json:"variableID" bson:"variableID"`
 	VariableValue string             `json:"variableValue" bson:"variableValue"`
+	CreatedDate   int64              `json:"createdDate" bson:"createdDate"`
+	CreatedBy     string             `json:"createdBy" bson:"createdBy"`
+	UpdatedDate   int64              `json:"updatedDate" bson:"updatedDate"`
+	UpdatedBy     string             `json:"updatedBy" bson:"updatedBy"`
 }
 
 type GlobalVariableRequest struct {
 	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	VariableID    string             `json:"variableID" bson:"variableID"`
 	VariableValue string             `json:"variableValue" bson:"variableValue"`
+	CreatedBy     string             `json:"createdBy" bson:"createdBy"`
 }
 
 type GlobalVariableUpdateRequest struct {
 	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	VariableID    string             `json:"variableID" bson:"variableID"`
 	VariableValue string             `json:"variableValue" bson:"variableValue"`
+	UpdatedBy     string             `json:"updatedBy" bson:"updatedBy"`
 }
